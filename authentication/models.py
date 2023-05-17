dea50b13db00f9fd3662225f75ca22df0f463a85
Ivan <krapivin.ivan2702@gmail.com>
2023-05-18 02:14:26 +0300

    auth fixed

--- authentication/models.py
+++ authentication/models.py
@@ -3,14 +3,14 @@ from django.contrib.auth.models import AbstractBaseUser,PermissionsMixin
 from authentication.managers import UserManager
 
 class User(AbstractBaseUser,PermissionsMixin):
-    firstname = models.CharField(verbose_name="Имя",max_length=255, blank=True)
-    avatar=models.ImageField(verbose_name="Аватар",upload_to='user_image', default=False, null = True)
-    gender=models.CharField(verbose_name="Пол",max_length=255, null = True)
-    height=models.CharField(verbose_name="Рост",max_length=255, null = True)
-    weight=models.CharField(verbose_name="Вес",max_length=255, null = True)
-    age=models.CharField(verbose_name="Возраст",max_length=255, null = True)
-    preparation=models.FloatField(verbose_name="Подготовка",default=1.0, null = True)
-    email=models.EmailField(verbose_name="Email",max_length=255,unique=True, null = True)
+    firstname = models.CharField(verbose_name="Имя",max_length=255)
+    avatar=models.ImageField(verbose_name="Аватар", upload_to='user_image', default=False, blank=True)
+    gender=models.CharField(verbose_name="Пол",max_length=255, null = True, blank=True)
+    height=models.CharField(verbose_name="Рост",max_length=255, null = True, blank=True)
+    weight=models.CharField(verbose_name="Вес",max_length=255, null = True, blank=True)
+    age=models.CharField(verbose_name="Возраст",max_length=255, null = True, blank=True)
+    preparation=models.FloatField(verbose_name="Подготовка",default=1.0, null = True, blank=True)
+    email=models.EmailField(verbose_name="Email",max_length=255,unique=True)
 
     is_active =models.BooleanField(verbose_name='Активирован',default=False)
     is_staff=models.BooleanField(verbose_name='Персонал',default=False)
