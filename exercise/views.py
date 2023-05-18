55bb5d330d051aceb5b45b8eb7e656fef74293bf
Ivan <krapivin.ivan2702@gmail.com>
2023-05-18 03:10:27 +0300

    CRUD - Done

--- exercise/views.py
+++ exercise/views.py
@@ -1,7 +1,15 @@
+from rest_framework.permissions import BasePermission
 from rest_framework.viewsets import ModelViewSet
 from exercise.models import Exercise
 from exercise.serializers import ExerciseSerializer
 
+class IsAdminOrReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        if request.method in ['PUT', 'PATCH', 'DELETE']:
+            return request.user.is_superuser
+        return True
+
 class  ExerciseViewset(ModelViewSet):
     queryset=Exercise.objects.all()
     serializer_class=ExerciseSerializer
+    permission_classes = [IsAdminOrReadOnly]
--- workout/views.py
+++ workout/views.py
@@ -1,7 +1,15 @@
+from rest_framework.permissions import BasePermission
 from rest_framework.viewsets import ModelViewSet
 from workout.models import Workout
 from workout.serializers import WorkoutSerializer
 
+class IsAdminOrReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        if request.method in ['PUT', 'PATCH', 'DELETE']:
+            return request.user.is_superuser
+        return True
+
 class  WorkoutViewset(ModelViewSet):
     queryset=Workout.objects.all()
     serializer_class=WorkoutSerializer
+    permission_classes = [IsAdminOrReadOnly]
